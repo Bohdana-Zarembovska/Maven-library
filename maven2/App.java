972bbc0ef36367132de0141c1ab697d80d5ced0c
Bohdana-Zarembovska <zarembovskabohdana@gmail.com>
2023-02-25 17:38:54 +0200

    Update App.java

--- maven2/App.java
+++ maven2/App.java
@@ -1,1 +1,17 @@
+package com.epizy.zarembovska.maven2;
+import java.util.Arrays;
+import java.util.stream.Stream;
 
+import com.epizy.zarembovska.mavenlab.Application;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	String[] array = {"Java", "javaiscool"};
+		Stream<String> streamOfArray = Arrays.stream(array); 
+		Application.findCharacters(streamOfArray);
+     
+    }
+}
