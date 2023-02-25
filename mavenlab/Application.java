8118ba265e04c6fa83a73ac6d897be474fb619fc
Bohdana-Zarembovska <zarembovskabohdana@gmail.com>
2023-02-25 17:29:02 +0200

    Add files via upload

--- mavenlab/Application.java
+++ mavenlab/Application.java
@@ -1,1 +1,15 @@
+package com.epizy.zarembovska.mavenlab;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+
+public class Application {
+    public static void findCharacters(Stream<String> stringlist){
+    	
+    	stringlist.map(s->s.split("")) 
+		        .flatMap(Arrays::stream).distinct() 
+		        .collect(Collectors.toList()).forEach(System.out::println);
+		
+    }
+}
